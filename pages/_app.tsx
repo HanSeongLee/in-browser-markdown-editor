dcee14eb67424d5cc137cc9e6204ed49fa2c4120
HanSeongLee <jkhanseong@gmail.com>
2023-04-08 15:45:39 +0900

    ADD | Font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,35 @@
-import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
+import '../styles/globals.scss';
+import type { AppProps } from 'next/app';
+import { Roboto, Roboto_Slab, Roboto_Mono } from 'next/font/google';
+
+const roboto = Roboto({
+  weight: ['300', '400', '500'],
+  subsets: ['latin'],
+});
+
+const robotoSlab = Roboto_Slab({
+  weight: ['300', '700'],
+  subsets: ['latin'],
+});
+
+const robotoMono = Roboto_Mono({
+  weight: ['400'],
+  subsets: ['latin'],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+      <>
+        <style jsx global>{`
+          :root {
+            --roboto-font: ${roboto.style.fontFamily};
+            --roboto-slab-font: ${robotoSlab.style.fontFamily};
+            --roboto-mono-font: ${robotoMono.style.fontFamily};
+          }
+        `}</style>
+        <Component {...pageProps} />
+      </>
+  );
 }
 
 export default MyApp
