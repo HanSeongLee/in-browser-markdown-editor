b4bf093f536aa5724e96732403225c97237e1128
HanSeongLee <jkhanseong@gmail.com>
2023-04-11 17:51:24 +0900

    ADD | Icons to Button component

--- components/commons/Button/index.tsx
+++ components/commons/Button/index.tsx
@@ -5,10 +5,12 @@ import DeleteIcon from '/public/icons/icon-delete.svg';
 import SaveIcon from '/public/icons/icon-save.svg';
 import MenuIcon from '/public/icons/icon-menu.svg';
 import CloseIcon from '/public/icons/icon-close.svg';
+import ShowPreviewIcon from '/public/icons/icon-show-preview.svg';
+import HidePreviewIcon from '/public/icons/icon-hide-preview.svg';
 
 interface IProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     variant?: 'primary' | 'default' | 'menu';
-    icon?: 'delete' | 'save' | 'menu' | 'close';
+    icon?: 'delete' | 'save' | 'menu' | 'close' | 'show-preview' | 'hide-preview';
 }
 
 const Button: React.FC<IProps> = ({ variant, icon, className, children, ...props }) => {
@@ -24,6 +26,8 @@ const Button: React.FC<IProps> = ({ variant, icon, className, children, ...props
             {icon === 'save' && (<SaveIcon />)}
             {icon === 'menu' && (<MenuIcon />)}
             {icon === 'close' && (<CloseIcon />)}
+            {icon === 'show-preview' && (<ShowPreviewIcon />)}
+            {icon === 'hide-preview' && (<HidePreviewIcon />)}
 
             {children}
         </button>
