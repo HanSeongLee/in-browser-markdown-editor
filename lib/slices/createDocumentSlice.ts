6251600ef6833e33297b48bec2fbac3618b4e837
HanSeongLee <jkhanseong@gmail.com>
2023-04-16 17:21:01 +0900

    ADD | Document slice and store

---
+++ lib/slices/createDocumentSlice.ts
@@ -0,0 +1,72 @@
+import { StateCreator } from 'zustand';
+import produce from 'immer';
+import moment from 'moment';
+
+export interface DocumentSlice {
+    documents: IDocument[];
+    findDocumentById: (id: number) => IDocument | undefined;
+    addDocument: (name: string) => number;
+    deleteDocument: (id: number) => void;
+    updateDocument: (id: number, {name, content}: {name: string, content: string}) => void;
+}
+
+export const createDocumentSlice: StateCreator<DocumentSlice> = (set, get) => {
+    let lastId = 0;
+
+    const generateId = () => {
+        return ++lastId;
+    };
+
+    return {
+        documents: [{
+            id: generateId(),
+            'name': 'untitled-document.md',
+            'content': '',
+            createdAt: '04-01-2022'
+        }, {
+            id: generateId(),
+            name: 'welcome.md',
+            content: '# Welcome to Markdown\n\nMarkdown is a lightweight markup language that you can use to add formatting elements to plaintext text documents.\n\n## How to use this?\n\n1. Write markdown in the markdown editor window\n2. See the rendered markdown in the preview window\n\n### Features\n\n- Create headings, paragraphs, links, blockquotes, inline-code, code blocks, and lists\n- Name and save the document to access again later\n- Choose between Light or Dark mode depending on your preference\n\n> This is an example of a blockquote. If you would like to learn more about markdown syntax, you can visit this [markdown cheatsheet](https://www.markdownguide.org/cheat-sheet/).\n\n#### Headings\n\nTo create a heading, add the hash sign (#) before the heading. The number of number signs you use should correspond to the heading level. You\'ll see in this guide that we\'ve used all six heading levels (not necessarily in the correct way you should use headings!) to illustrate how they should look.\n\n##### Lists\n\nYou can see examples of ordered and unordered lists above.\n\n###### Code Blocks\n\nThis markdown editor allows for inline-code snippets, like this: `<p>I\'m inline</p>`. It also allows for larger code blocks like this:\n\n```\n<main>\n  <h1>This is a larger code block</h1>\n</main>\n```',
+            createdAt: '04-01-2022'
+        }],
+        findDocumentById: (id: number) => {
+            const { documents } = get();
+            return documents.find(({ id: _id }) => _id === id);
+        },
+        addDocument: (name: string) => {
+            const id = generateId();
+            const createdAt = moment().format('MM-DD-YYYY');
+            set(
+                produce((draft) => {
+                    draft.documents.push({
+                        id,
+                        name,
+                        createdAt,
+                        content: '',
+                    });
+                })
+            );
+            return id;
+        },
+        deleteDocument: (id: number) => {
+            set(
+                produce((draft) => {
+                    draft.documents = draft.documents.filter(({ id: _id }: { id: number }) => _id !== id);
+                })
+            );
+        },
+        updateDocument: (id: number, { name, content }: { name: string, content: string }) => {
+            set(
+                produce((draft) => {
+                    const document = draft.documents.find(({ id: _id }: { id: number }) => _id === id);
+                    if (name) {
+                        document.name = name;
+                    }
+                    if (content) {
+                        document.content = content;
+                    }
+                })
+            );
+        },
+    };
+};
--- lib/store.ts
+++ lib/store.ts
@@ -1,12 +1,14 @@
 import { createSidebarSlice, SidebarSlice } from 'lib/slices/createSidebarSlice';
 import { create } from 'zustand';
 import { createModalSlice, ModalSlice } from 'lib/slices/createModalSlice';
+import { createDocumentSlice, DocumentSlice } from 'lib/slices/createDocumentSlice';
 
-type StoreState = SidebarSlice & ModalSlice;
+type StoreState = SidebarSlice & ModalSlice & DocumentSlice;
 
 export const useAppStore = create<StoreState>()(
     (...a) => ({
         ...createSidebarSlice(...a),
         ...createModalSlice(...a),
+        ...createDocumentSlice(...a),
     })
 );
