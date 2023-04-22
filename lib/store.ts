581b4e0dc7d4450ed5b36b26abb6bca209cf41c3
HanSeongLee <jkhanseong@gmail.com>
2023-04-22 16:56:23 +0900

    ADD | Persist to app store

--- lib/store.ts
+++ lib/store.ts
@@ -2,13 +2,20 @@ import { createSidebarSlice, SidebarSlice } from 'lib/slices/createSidebarSlice'
 import { create } from 'zustand';
 import { createModalSlice, ModalSlice } from 'lib/slices/createModalSlice';
 import { createDocumentSlice, DocumentSlice } from 'lib/slices/createDocumentSlice';
+import { persist } from 'zustand/middleware';
 
 type StoreState = SidebarSlice & ModalSlice & DocumentSlice;
 
 export const useAppStore = create<StoreState>()(
-    (...a) => ({
+    persist((...a) => ({
         ...createSidebarSlice(...a),
         ...createModalSlice(...a),
         ...createDocumentSlice(...a),
+    }), {
+        name: 'in-browser-markdown-editor-store',
+        version: 1,
+        partialize: (state) => ({
+            documents: state.documents,
+        }),
     })
 );
