fc554f4170f153b5205b93ccbfab86c3df31b4ee
HanSeongLee <jkhanseong@gmail.com>
2023-04-23 21:55:25 +0900

    FIX | Prevent zooming on Safari

--- components/commons/DefaultHead/index.tsx
+++ components/commons/DefaultHead/index.tsx
@@ -12,7 +12,7 @@ const DefaultHead: React.FC<IProps> = ({ title, description, url, ogi }) => {
         <>
             <meta charSet="UTF-8"/>
             <meta name="viewport"
-                  content="width=device-width, initial-scale=1.0"
+                  content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
             />
 
             <title>{title}</title>
